a1b64a6a14a188452aec9c702159105fd06aab08
king353 <3180587913@qq.com>
2020-12-28 10:05:05 +0800

    hello

---
+++ ok8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void){
+<<<<<<< HEAD
+	printf("hello sunday,I'm fine");
+=======
+	printf("hello sunday,I'm fine,hahaha");
+<<<<<<< HEAD
+>>>>>>> e51f5a0a4595c397becadf46bb8056323bfade2b
+=======
+>>>>>>> main
+>>>>>>> 7e77e4544434a684ec217d9b3e4e27fb63f2de63
+}
